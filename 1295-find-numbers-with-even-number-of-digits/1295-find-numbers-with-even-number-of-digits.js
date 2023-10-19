20c651018dcb93386065f8084a7f485cd38630b3
Ibrar Alam <88775949+ibraralam22@users.noreply.github.com>
2023-10-19 23:39:42 +0530

    Time: 248 ms (5.94%), Space: 51 MB (6.42%) - LeetHub

---
+++ 1295-find-numbers-with-even-number-of-digits/1295-find-numbers-with-even-number-of-digits.js
@@ -0,0 +1,35 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findNumbers = function(nums) {
+    let c = 0;
+    let fun = (a) =>{
+    let b = '';
+      b += a;
+      return b;
+    }
+
+    let splitc = (b) =>{
+      let arr = [];
+      for(let i=0;i<b.length; i++)
+        {
+          arr.push(b[i]);
+        }
+      return arr;
+    }
+
+    for(let i =0; i<nums.length; i++)
+      {
+        let str = fun(nums[i]);
+        console.log(str)
+        let len = splitc(str);
+        console.log(len);
+        if(len.length%2==0)
+        {
+          c++;
+        }
+
+      }
+    return c;
+};
