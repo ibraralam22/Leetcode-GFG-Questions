bd0ebbe27d51b1c311bf9236da26262987ae8bf4
Ibrar Alam <88775949+ibraralam22@users.noreply.github.com>
2024-07-22 14:17:29 +0530

    Time: 125 ms (76.93%), Space: 57.7 MB (46.11%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.js
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -3,15 +3,9 @@
  * @return {boolean}
  */
 var isPalindrome = function(x) {
-    let ans = 0;
     if(x<0) return false;
-    let n = x;
-    while(x!=0){
-        let digit = x % 10;
-        ans = ans * 10 + digit;
-        x = Math.floor(x / 10)
-    }
+    let n=x;
+    let ans = parseInt(x.toString().split("").reverse().join(""));
     if(ans != n) return false;
     return true;
-    
 };
